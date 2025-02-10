039815cfc2abce69ba53c14be08d759f9d1f0337
David Uhlig <david.uhlig@gmail.com>
2025-02-10 16:02:49 +0100

    Configure google oauth initializer

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,13 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  configure do |config|
+    config.logger = Rails.logger if Rails.env.development?
+  end
+
+  provider :google_oauth2,
+           Rails.application.credentials.dig(:google, :client_id),
+           Rails.application.credentials.dig(:google, :client_secret),
+           {
+             include_granted_scopes: true,
+             scope: "email, profile, offline"
+           }
+end
